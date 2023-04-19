65419f817d75f87e6bafd0a315cada0a9efed181
Lam Manh Hung <hunglmhe171165@fpt.edu.vn>
2023-04-19 14:01:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './screens/login';
+import ChooseAction from './screens/admin/ChooseAction';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path='/login' element = {<Login/>}>
+          </Route>
+          <Route paht='/' element = {<ChooseAction/>}>
+          </Route>
+        </Routes>
+    </Router>
+
   );
 }
 
