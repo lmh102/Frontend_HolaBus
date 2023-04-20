28a183f4ae4ac058b211e6091da2ce1a605a0b11
AoGS-tech <110282191+AoGS-tech@users.noreply.github.com>
2023-04-20 11:31:41 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import LoginForm from './screens/login';
+
 function App() {
   const navigate = useNavigate();
   useEffect(() => {
       navigate('/login');
   },[])
   return (
-      <></>
-
+    <></>
   );
+    
 }
 
 export default App;
